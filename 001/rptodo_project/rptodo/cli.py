59ad2b8854d9f8fffd8b396d0911e6fb63d05290
Alex Shifrin <alex.shifrin@gmail.com>
2022-01-02 15:27:25 -0800

    Add clear command to CLI

--- 001/rptodo_project/rptodo/cli.py
+++ 001/rptodo_project/rptodo/cli.py
@@ -168,6 +168,28 @@ def remove(
         else:
             typer.echo("Operation canceled")
 
+@app.command(name="clear")
+def remove_all(
+    force: bool = typer.Option(
+        ...,
+        prompt="Delete all to-dos?",
+        help="Force deletion without confirmation.",
+    ),
+) -> None:
+    """Remove all to-dos."""
+    todoer = get_todoer()
+    if force:
+        error = todoer.remove_all().error
+        if error:
+            typer.secho(
+                f'Removing to-dos failed with "{ERRORS[error]}"',
+                fg=typer.colors.RED,
+            )
+            raise typer.Exit(1)
+        else:
+            typer.secho("All to-dos were remove", fg=typer.colors.GREEN)
+    else:
+        typer.echo("Operation canceled")
 
 def _version_callback(value: bool) -> None:
     if value:
