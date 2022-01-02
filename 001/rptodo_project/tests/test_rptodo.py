308849de6cba0fcb0c12d7c28a4866203c8d58a3
Alex Shifrin <alex.shifrin@gmail.com>
2022-01-01 17:57:19 -0800

    Fix another == typo :(

--- 001/rptodo_project/tests/test_rptodo.py
+++ 001/rptodo_project/tests/test_rptodo.py
@@ -8,6 +8,5 @@ runner = CliRunner()
 
 def test_version():
     result = runner.invoke(cli.app, ["--version"])
-    assert result.exit_code = 0
+    assert result.exit_code == 0
     assert f"{__app_name__} v{__version__}\n" in result.stdout
-    
