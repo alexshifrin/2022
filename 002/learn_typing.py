d1b83d7b55a32ea8497a7888959b047f5e8f578a
Alex Shifrin <alex.shifrin@gmail.com>
2022-01-03 17:00:43 -0800

    Add annotation comments

--- 002/learn_typing.py
+++ 002/learn_typing.py
@@ -1,9 +1,11 @@
 import math
 reveal_type(math.pi)
 
+# can inspect with `circumference.__annotations__`
 def circumference(radius: float) -> float:
     return 2 * math.pi * radius
 
+# can inspect on module level with `__annotations__`
 circ = circumference(10)
 
 reveal_locals()
