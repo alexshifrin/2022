51f2e45ac7ec2d7bdcb73d201f2f9739e6a0cdd7
Alex Shifrin <alex.shifrin@gmail.com>
2022-01-04 22:08:58 -0800

    Add comparison to PlayingCard

--- 003/cards.py
+++ 003/cards.py
@@ -82,3 +82,25 @@ print(Deck())
 
 # priint metadata from Position object `seattle`
 print(fields(seattle)[2].metadata['unit'])
+
+from dataclasses import dataclass, field
+
+RANKS = '2 3 4 5 6 7 8 9 10 J Q K A'.split()
+SUITS = '♣ ♢ ♡ ♠'.split()
+
+@dataclass(order=True)
+class PlayingCard:
+    sort_index: int = field(init=False, repr=False)
+    rank: str
+    suit: str
+
+    def __post_init__(self):
+        self.sort_index = (RANKS.index(self.rank) * len(SUITS)
+                           + SUITS.index(self.suit))
+
+    def __str__(self):
+        return f'{self.suit}{self.rank}'
+
+queen_of_hearts = PlayingCard('Q', '♡')
+ace_of_spades = PlayingCard('A', '♠')
+print(ace_of_spades > queen_of_hearts)
