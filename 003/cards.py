c59af8a2045126011561e3bb6a799210bd02d558
Alex Shifrin <alex.shifrin@gmail.com>
2022-01-04 21:54:41 -0800

    Add method example

--- 003/cards.py
+++ 003/cards.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass, make_dataclass
 from typing import Any
+from math import asin, cos, radians, sin, sqrt
 
 @dataclass
 class PlayingCard:
@@ -19,8 +20,16 @@ print(ace_of_spades2)
 @dataclass
 class Position:
     name: str
-    lat: float = 0.0
     lon: float = 0.0
+    lat: float = 0.0
+
+    def distance_to(self, other):
+        r = 6371  # Earth radius in kilometers
+        lam_1, lam_2 = radians(self.lon), radians(other.lon)
+        phi_1, phi_2 = radians(self.lat), radians(other.lat)
+        h = (sin((phi_2 - phi_1) / 2)**2
+             + cos(phi_1) * cos(phi_2) * sin((lam_2 - lam_1) / 2)**2)
+        return 2 * r * asin(sqrt(h))
 
 seattle = Position("Seattle", 47.6, 122.3)
 
@@ -29,6 +38,9 @@ print(seattle)
 nowhere = Position("Nowhere")
 
 print(nowhere)
+
+print("Distance from Seattle to nowhere" \
+        f" is {seattle.distance_to(nowhere)}!")
 
 @dataclass
 class Anything:
