c33de31963514dafd2ec2f5030a7e75937606f05
Alex Shifrin <alex.shifrin@gmail.com>
2022-01-04 21:50:43 -0800

    Add default value example

--- 003/cards.py
+++ 003/cards.py
@@ -14,3 +14,17 @@ PlayingCard2 = make_dataclass('PlayingCard2', ['rank', 'suit'])
 ace_of_spades2 = PlayingCard2('Ace', 'Spades')
 
 print(ace_of_spades2)
+
+@dataclass
+class Position:
+    name: str
+    lat: float = 0.0
+    lon: float = 0.0
+
+seattle = Position("Seattle", 47.6, 122.3)
+
+print(seattle)
+
+nowhere = Position("Nowhere")
+
+print(nowhere)
