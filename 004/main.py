d7530fbda0c56aae30e4da8187fcc878717557a9
Alex Shifrin <alex.shifrin@gmail.com>
2022-01-05 18:17:14 -0800

    Use fixed parameters before variable ones

--- 004/main.py
+++ 004/main.py
@@ -18,3 +18,11 @@ async def root():
 @app.get("/item/{item_id}")
 async def show_item(item_id: int):
     return {"item_id": item_id}
+
+@app.get("/users/me")
+async def read_user_me():
+    return {"user_id": "the current user"}
+
+@app.get("/users/{user_id}")
+async def read_user(user_id: int):
+    return {"user_id": user_id}
