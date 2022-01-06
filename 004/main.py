89561ea0ecb47ad9272721fab545d5102204b141
Alex Shifrin <alex.shifrin@gmail.com>
2022-01-05 21:14:58 -0800

    Use pydantic model in items function

--- 004/main.py
+++ 004/main.py
@@ -51,4 +51,8 @@ async def read_user(user_id: int):
 
 @app.post("/items/")
 async def create_item(item: Item):
-    return item
+    item_dict = item.dict()
+    if item.tax:
+        price_with_tax = item.price + item.tax
+        item_dict.update({"price_with_tax": price_with_tax})
+    return item_dict
