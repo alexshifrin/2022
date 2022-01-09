e2b514064596eb8fcd74bbe0b9988d20b821c8aa
Alex Shifrin <alex.shifrin@gmail.com>
2022-01-09 14:02:46 -0800

    Add styling to display

--- 005/app.py
+++ 005/app.py
@@ -8,15 +8,32 @@ data = data.query("type == 'conventional' and region == 'Albany'")
 data["Date"] = pd.to_datetime(data["Date"], format="%Y-%m-%d")
 data.sort_values("Date", inplace=True)
 
+external_stylesheets = [
+    {
+        "href": "https://fonts.googleapis.com/css2?"
+        "family=Lato:wght@400;700&display=swap",
+        "rel": "stylesheet",
+    },
+]
+
 app = Dash(__name__)
+app.title = "Avocado Analytics: Understand Your Avocados!"
 
 app.layout = html.Div(
     children=[
-        html.H1(children="Avocado Analytics",),
-        html.P(
-            children="Analyze the behavoir of avocado prices"
-            " and the number of avocados sold in the US"
-            " between 2015 and 2018"
+        html.Div(
+            children=[
+                html.P(children="🥑", className="header-emoji"),
+                html.H1(children="Avocado Analytics",
+                className="header-title"),
+                html.P(
+                    children="Analyze the behavoir of avocado prices"
+                    " and the number of avocados sold in the US"
+                    " between 2015 and 2018",
+                    className="header-description",
+                ),
+            ],
+            className="header",
         ),
         dcc.Graph(
             figure={
