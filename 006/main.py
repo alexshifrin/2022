c991070582163411f81beb9c9f344426b56dc4aa
Alex Shifrin <alex.shifrin@gmail.com>
2022-01-11 09:02:29 -0800

    Update POST /posts endpoint with 201 status code

--- 006/main.py
+++ 006/main.py
@@ -28,7 +28,7 @@ def root():
 def get_posts():
     return {"data": my_posts}
 
-@app.post("/posts")
+@app.post("/posts", status_code=status.HTTP_201_CREATED)
 def create_posts(post: Post):
     post_dict = post.dict()
     post_dict["id"] = max([post["id"] for post in my_posts]) + 1
