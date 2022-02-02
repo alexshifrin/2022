44a8638e7c49d170d00af9f98d355e99c0b13a63
Alex Shifrin <alex.shifrin@gmail.com>
2022-02-02 09:16:26 -0800

    Remove SQL code replaced by SQLAlchemy

--- 006/app/main.py
+++ 006/app/main.py
@@ -38,12 +38,6 @@ while True:
         print("Error: ", error)
         time.sleep(2)
 
-
-my_posts = [
-    {"title": "post 1 title", "content": "post 1 content", "id": 1},
-    {"title": "post 2 title", "content": "post 2 content", "id": 2}
-]
-
 def _find_post(id):
     for index, post in enumerate(my_posts):
         if post["id"] == id:
@@ -59,20 +53,12 @@ def root():
 # GET /posts
 @app.get("/posts")
 def get_posts(db: Session = Depends(get_db)):
-    # cursor.execute("""SELECT * FROM posts ORDER BY id ASC;""")
-    # posts = cursor.fetchall()
     posts = db.query(models.Post).all()
     return {"data": posts}
 
 # POST /posts
 @app.post("/posts", status_code=status.HTTP_201_CREATED)
 def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db)):
-    # cursor.execute(
-    #     """INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING *;""",
-    #     (post.title, post.content, post.published)
-    # )
-    # new_post = cursor.fetchone()
-    # conn.commit()
     new_post = models.Post(**post.dict())
     db.add(new_post)
     db.commit()
@@ -82,11 +68,7 @@ def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db)):
 # GET /posts/{id}
 @app.get("/posts/{id}")
 def get_post(id: int, db: Session = Depends(get_db)):
-    # cursor.execute("""SELECT * FROM posts WHERE id = %s;""", (id,))
-    # post = cursor.fetchone()
-
     matched_post = db.query(models.Post).filter(models.Post.id == id).first()
-
     if not matched_post:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
@@ -97,21 +79,6 @@ def get_post(id: int, db: Session = Depends(get_db)):
 # DELETE /posts/{id}
 @app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_post(id: int, db: Session = Depends(get_db)):
-    # index, deleted_post = _find_post(id)
-    # if not deleted_post:
-    #     raise HTTPException(
-    #         status_code=status.HTTP_404_NOT_FOUND,
-    #         detail=f"Post with id {id} not found or deleted",
-    #     )
-    # global my_posts
-    # my_posts.pop(index)
-    # # my_posts = [post for post in my_posts if post["id"] != id]
-    # return Response(status_code=status.HTTP_204_NO_CONTENT)
-
-    # cursor.execute("""DELETE FROM posts WHERE id = %s RETURNING *;""", (id,))
-    # deleted_post = cursor.fetchone()
-    # conn.commit()
-
     delete_post_query = db.query(models.Post).filter(models.Post.id == id)
     if not delete_post_query.first():
         raise HTTPException(
@@ -125,30 +92,13 @@ def delete_post(id: int, db: Session = Depends(get_db)):
 # PUT /posts/{id}
 @app.put("/posts/{id}")
 def update_post(id: int, post: schemas.PostCreate, db: Session = Depends(get_db)):
-    # cursor.execute(
-    #     """UPDATE posts SET title = %s, content = %s, published = %s WHERE id = %s RETURNING *;""",
-    #     (post.title, post.content, post.published, id)
-    # )
-    # updated_post = cursor.fetchone()
-    # conn.commit()
-
     update_post_query = db.query(models.Post).filter(models.Post.id == id)
-
     updated_post = update_post_query.first()
-
     if updated_post is None:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
             detail=f"Post with id {id} not found to update",
         )
-    
     update_post_query.update(post.dict(), synchronize_session=False)
     db.commit()
-
     return {"message": "updated post!", "post": update_post_query.first()}
-
-@app.get("/sqlalchemy")
-def test_posts(db: Session = Depends(get_db)):
-    posts = db.query(models.Post).all()
-    # print(db.query(models.Post)) = "SELECT * FROM posts;"
-    return {"data": posts}
