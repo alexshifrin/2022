065394d31e0f4a0a68f520548299a70c606f4e22
Alex Shifrin <alex.shifrin@gmail.com>
2022-02-11 12:41:46 -0800

    Change auth to use OAuth2PasswordRequestForm

--- 006/app/routers/auth.py
+++ 006/app/routers/auth.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, Depends, status, HTTPException, Response
+from fastapi.security.oauth2 import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 
 from .. import database, models, oauth2, schemas, utils
@@ -7,8 +8,8 @@ from .. import database, models, oauth2, schemas, utils
 router = APIRouter(tags=["Authentication"])
 
 @router.post("/login")
-def login(user_credentials: schemas.UserLogin, db: Session = Depends(database.get_db)):
-    user = db.query(models.User).filter(models.User.email == user_credentials.email).first()
+def login(user_credentials: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(database.get_db)):
+    user = db.query(models.User).filter(models.User.email == user_credentials.username).first()
     if not user:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Invalid credentials")
     if not utils.verify(user_credentials.password, user.password):
