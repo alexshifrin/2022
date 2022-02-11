f7af26d562baa1ac1d0c80c4d57102e3365705e8
Alex Shifrin <alex.shifrin@gmail.com>
2022-02-11 12:56:11 -0800

    Create get_current_user in oauth2

--- 006/app/oauth2.py
+++ 006/app/oauth2.py
@@ -1,7 +1,13 @@
 from datetime import datetime, timedelta
 
+from fastapi import Depends, status, HTTPException
+from fastapi.security.oauth2 import OAuth2PasswordBearer
 from jose import JWTError, jwt
 
+from . import schemas
+
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 
 # openssl rand -hex 32
 SECRET_KEY = "9a3ec03db7789f636327c2af28f25adb1507c78db74e8b4f718ca4757b94416d"
@@ -14,3 +20,21 @@ def create_access_token(data: dict):
     to_encode.update({"exp": expiration})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
+
+def verify_access_token(token: str, credentials_exception):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=ALGORITHM)
+        id: str = payload.get("user_id")
+        if id is None:
+            raise credentials_exception
+        token_data = schemas.TokenData(id=id)
+    except JWTError:
+        raise credentials_exception
+
+def get_current_user(token: str = Depends(oauth2_scheme)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"}
+    )
+    return verify_access_token(token, credentials_exception)
