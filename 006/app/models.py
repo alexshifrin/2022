eb8e131c4aaa51bd8cdc78ac4aec4e241b75705c
Alex Shifrin <alex.shifrin@gmail.com>
2022-03-07 09:30:05 -0800

    Add User relationship to Post model and update schema to include owner

--- 006/app/models.py
+++ 006/app/models.py
@@ -3,6 +3,7 @@ from .database import Base
 from sqlalchemy import TIMESTAMP, Column, ForeignKey, Integer, String, Boolean, null, text
 # from sqlalchemy.sql.sqltypes import TIMESTAMP
 # from sqlalchemy.sql.expression import text
+from sqlalchemy.orm import relationship
 
 class Post(Base):
     __tablename__ = "posts"
@@ -11,7 +12,11 @@ class Post(Base):
     content = Column(String, nullable=False)
     published = Column(Boolean, nullable=False, server_default="TRUE")
     created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text("NOW()"))
+    # references name of table and column in Postgres
     owner_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
+
+    # references name of Python object
+    owner = relationship("User")
 
 class User(Base):
     __tablename__ = "users"
--- 006/app/schemas.py
+++ 006/app/schemas.py
@@ -4,25 +4,6 @@ from typing import Optional
 from pydantic import BaseModel, EmailStr
 
 
-class PostBase(BaseModel):
-    title: str
-    content: str
-    published: bool = True
-
-
-class PostCreate(PostBase):
-    pass
-
-
-class PostResponse(PostBase):
-    id: int
-    created_at: datetime
-    owner_id: int
-
-    class Config:
-        orm_mode = True
-
-
 class UserCreate(BaseModel):
     email: EmailStr
     password: str
@@ -40,6 +21,26 @@ class UserOut(BaseModel):
 class UserLogin(BaseModel):
     email: EmailStr
     password: str
+
+
+class PostBase(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+
+
+class PostCreate(PostBase):
+    pass
+
+
+class PostResponse(PostBase):
+    id: int
+    created_at: datetime
+    owner_id: int
+    owner: UserOut
+
+    class Config:
+        orm_mode = True
 
 
 class Token(BaseModel):
