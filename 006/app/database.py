71cddaec93d95989438728d2613b1d4c75932b23
Alex Shifrin <alex.shifrin@gmail.com>
2022-03-29 16:17:38 -0700

    Use config.settings in database.py

--- 006/app/database.py
+++ 006/app/database.py
@@ -7,7 +7,11 @@ from sqlalchemy.orm import sessionmaker
 import psycopg2
 from psycopg2.extras import RealDictCursor
 
-SQLALCHEMY_DATABASE_URL = "postgresql://shifty:@localhost:5432/fastapi"
+from .config import settings
+
+# SQLALCHEMY_DATABASE_URL = "postgresql://shifty:@localhost:5432/fastapi"
+SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}"
+
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
