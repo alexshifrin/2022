2afe032938d42d52947b433a0c25004f370b3046
Alex Shifrin <alex.shifrin@gmail.com>
2022-04-05 21:54:06 -0700

    Add logic for voting on non-existent post

--- 006/app/routers/vote.py
+++ 006/app/routers/vote.py
@@ -9,6 +9,11 @@ router = APIRouter(prefix="/vote", tags=["Vote"])
 
 @router.post("/", status_code=status.HTTP_201_CREATED)
 def vote(vote: schemas.Vote, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
+    post_query = db.query(models.Post).filter(models.Post.id == vote.post_id)
+    found_post = post_query.first()
+    if not found_post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post {vote.post_id} does not exist")
+
     vote_query = db.query(models.Vote).filter(models.Vote.post_id == vote.post_id, models.Vote.user_id == current_user.id)
     found_vote = vote_query.first()
     if vote.dir == 1:
@@ -24,4 +29,3 @@ def vote(vote: schemas.Vote, db: Session = Depends(get_db), current_user: int = 
         vote_query.delete(synchronize_session=False)
         db.commit()
         return {"message": "successfully deleted vote"}
-        
