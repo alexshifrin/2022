67a76e9d78352982f8d33064a0672af2ff853272
Alex Shifrin <alex.shifrin@gmail.com>
2022-04-11 09:54:41 -0700

    Create autogenerate Alembic revision for user phone via model

--- 006/app/models.py
+++ 006/app/models.py
@@ -24,6 +24,7 @@ class User(Base):
     email = Column(String, nullable=False, unique=True)
     password = Column(String, nullable=False)
     created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text("NOW()"))
+    phone = Column(String)
 
 class Vote(Base):
     __tablename__ = "votes"
